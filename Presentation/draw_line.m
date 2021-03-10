d4168d9acc06f201461e00feb230e403ac683da3
zeyang23 <zeyang23@outlook.com>
2021-03-10 11:11:39 +0800

    bisection method

---
+++ Presentation/draw_line.m
@@ -0,0 +1,5 @@
+function draw_line(A,B)
+    x=[A(1),B(1)];
+    y=[A(2),B(2)];
+    plot(x,y,'b','LineWidth',1)
+end
---
+++ Presentation/draw_line2.m
@@ -0,0 +1,5 @@
+function draw_line2(A,B)
+    x=[A(1),B(1)];
+    y=[A(2),B(2)];
+    plot(x,y,'r','LineWidth',1)
+end
