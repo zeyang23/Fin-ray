d97748f699c43fc7bd576f39c4f63229e58e0173
zeyang23 <zeyang23@outlook.com>
2021-05-21 00:44:21 +0800

    ellipse experiment

--- Experiments/test_05_20_transfer.m
+++ Experiments/test_05_20_transfer.m
@@ -25,6 +25,7 @@ Fsensor_series=[];
 
 % 第3组数据 这组数据比较好
 % 错位对齐，1mm的误差可以有很大的影响
+% 这组数据可以使用
 
 % Fsensor_series(:,1)=[0.36;1.45];
 % Fsensor_series(:,2)=[0.39;2.92];
@@ -44,6 +45,7 @@ Fsensor_series=[];
 
 % 有刚性约束 r=30e-3 31.5
 % 第1组数据
+% 这组数据可以使用
 % Fsensor_series(:,1)=[0.30;1.15];
 % Fsensor_series(:,2)=[0.37;2.46];
 % Fsensor_series(:,3)=[0.30;4.19];
@@ -62,26 +64,69 @@ Fsensor_series=[];
 
 
 % 无刚性约束 r=30e-3 delta=40e-3
-Fsensor_series(:,1)=[0.58;0.94];
-Fsensor_series(:,2)=[0.86;2.19];
-Fsensor_series(:,3)=[1.24;3.64];
-Fsensor_series(:,4)=[1.56;5.14];
-Fsensor_series(:,5)=[2.10;6.40];
-Fsensor_series(:,6)=[2.70;7.59];
-Fsensor_series(:,7)=[3.28;8.57];
-Fsensor_series(:,8)=[4.19;9.09];
+% 这组数据可以使用
+% Fsensor_series(:,1)=[0.58;0.94];
+% Fsensor_series(:,2)=[0.86;2.19];
+% Fsensor_series(:,3)=[1.24;3.64];
+% Fsensor_series(:,4)=[1.56;5.14];
+% Fsensor_series(:,5)=[2.10;6.40];
+% Fsensor_series(:,6)=[2.70;7.59];
+% Fsensor_series(:,7)=[3.28;8.57];
+% Fsensor_series(:,8)=[4.19;9.09];
+% 
+% Fcal_norm_series=[];
+% Fcal_norm_series(:,1)=1.12;
+% Fcal_norm_series(:,2)=2.57;
+% Fcal_norm_series(:,3)=3.90;
+% Fcal_norm_series(:,4)=5.54;
+% Fcal_norm_series(:,5)=6.57;
+% Fcal_norm_series(:,6)=7.57;
+% Fcal_norm_series(:,7)=8.40;
+% Fcal_norm_series(:,8)=9.08;
+% 
+% alpha=110.2/180*pi;
+
+
+% 有刚性约束 椭圆a=20e-3 b=30e-3 21.5 31.5
+% 第1组数据 0:5:25
+% Fsensor_series(:,1)=[1.11;-0.28];
+% Fsensor_series(:,2)=[2.58;-0.18];
+% Fsensor_series(:,3)=[4.60;0.34];
+% Fsensor_series(:,4)=[7.16;0.90];
+% Fsensor_series(:,5)=[11.20;1.94];
+% Fsensor_series(:,6)=[17.40;2.61];
+% 
+% 
+% Fcal_norm_series=[];
+% Fcal_norm_series(:,1)=1.23;
+% Fcal_norm_series(:,2)=2.65;
+% Fcal_norm_series(:,3)=3.90;
+% Fcal_norm_series(:,4)=4.24;
+% Fcal_norm_series(:,5)=6.50;
+% Fcal_norm_series(:,6)=8.92;
+
+% 第2组数据 0:3:18
+% 这组数据可以使用
+Fsensor_series(:,1)=[0.70;-0.24];
+Fsensor_series(:,2)=[1.40;-0.42];
+Fsensor_series(:,3)=[2.22;-0.42];
+Fsensor_series(:,4)=[3.26;-0.21];
+Fsensor_series(:,5)=[4.52;0.11];
+Fsensor_series(:,6)=[6.04;0.52];
+
 
 Fcal_norm_series=[];
-Fcal_norm_series(:,1)=1.12;
-Fcal_norm_series(:,2)=2.57;
-Fcal_norm_series(:,3)=3.90;
-Fcal_norm_series(:,4)=5.54;
-Fcal_norm_series(:,5)=6.57;
-Fcal_norm_series(:,6)=7.57;
-Fcal_norm_series(:,7)=8.40;
-Fcal_norm_series(:,8)=9.08;
+Fcal_norm_series(:,1)=0.70;
+Fcal_norm_series(:,2)=1.51;
+Fcal_norm_series(:,3)=2.36;
+Fcal_norm_series(:,4)=3.27;
+Fcal_norm_series(:,5)=4.41;
+Fcal_norm_series(:,6)=5.50;
 
-alpha=110.2/180*pi;
+alpha=200.2/180*pi;
+
+
+
 
 F_trans_series=[];
 for i=1:size(Fsensor_series,2)
