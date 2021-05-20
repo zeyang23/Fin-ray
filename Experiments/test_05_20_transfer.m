93f7eacea2d3ca639d756a55ab3e3b336a211df1
zeyang23 <zeyang23@outlook.com>
2021-05-20 22:03:27 +0800

    no-rigid experiment

--- Experiments/test_05_20_transfer.m
+++ Experiments/test_05_20_transfer.m
@@ -42,24 +42,44 @@ Fsensor_series=[];
 % Fcal_norm_series(:,6)=13.58;
 
 
-% 有刚性约束 r=30e-3
+% 有刚性约束 r=30e-3 31.5
 % 第1组数据
-Fsensor_series(:,1)=[0.30;1.15];
-Fsensor_series(:,2)=[0.37;2.46];
-Fsensor_series(:,3)=[0.30;4.19];
-Fsensor_series(:,4)=[0.25;6.34];
-Fsensor_series(:,5)=[0.35;9.00];
-Fsensor_series(:,6)=[0.80;12.60];
+% Fsensor_series(:,1)=[0.30;1.15];
+% Fsensor_series(:,2)=[0.37;2.46];
+% Fsensor_series(:,3)=[0.30;4.19];
+% Fsensor_series(:,4)=[0.25;6.34];
+% Fsensor_series(:,5)=[0.35;9.00];
+% Fsensor_series(:,6)=[0.80;12.60];
 
 % Fcal_norm_series=[];
-Fcal_norm_series(:,1)=1.24;
-Fcal_norm_series(:,2)=2.67;
-Fcal_norm_series(:,3)=4.24;
-Fcal_norm_series(:,4)=6.06;
-Fcal_norm_series(:,5)=8.33;
-Fcal_norm_series(:,6)=12.81;
+% Fcal_norm_series(:,1)=1.24;
+% Fcal_norm_series(:,2)=2.67;
+% Fcal_norm_series(:,3)=4.24;
+% Fcal_norm_series(:,4)=6.06;
+% Fcal_norm_series(:,5)=8.33;
+% Fcal_norm_series(:,6)=12.81;
 
 
+
+% 无刚性约束 r=30e-3 delta=40e-3
+Fsensor_series(:,1)=[0.58;0.94];
+Fsensor_series(:,2)=[0.86;2.19];
+Fsensor_series(:,3)=[1.24;3.64];
+Fsensor_series(:,4)=[1.56;5.14];
+Fsensor_series(:,5)=[2.10;6.40];
+Fsensor_series(:,6)=[2.70;7.59];
+Fsensor_series(:,7)=[3.28;8.57];
+Fsensor_series(:,8)=[4.19;9.09];
+
+Fcal_norm_series=[];
+Fcal_norm_series(:,1)=1.12;
+Fcal_norm_series(:,2)=2.57;
+Fcal_norm_series(:,3)=3.90;
+Fcal_norm_series(:,4)=5.54;
+Fcal_norm_series(:,5)=6.57;
+Fcal_norm_series(:,6)=7.57;
+Fcal_norm_series(:,7)=8.40;
+Fcal_norm_series(:,8)=9.08;
 
 alpha=110.2/180*pi;
 
